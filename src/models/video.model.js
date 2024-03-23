a03f615def358fe2ce7e7f0867be0db04ce3e152
Ashutosh Pandey <ashupandey1620@gmail.com>
2024-03-23 18:49:09 +0530

    Models for Data saving in DB

---
+++ src/models/video.model.js
@@ -0,0 +1,39 @@
+import mongoose, {Schema} from "mongoose";
+
+const VideoSchema = new Schema(
+    {
+        videoFile: {
+            type: String,
+            required: true,
+        },
+        thumbNail:{
+            type: String,
+            required:true
+        },
+        title:{
+            type: String,
+            required:true
+        },
+        description:{
+            type: String,
+            required:true
+        },
+        duration:{
+            type:Number,
+            required:true
+        },
+        views:{
+            type:Number,
+            required:true
+        },
+        isPublished:{
+            type:Boolean,
+            default:true
+        },
+        owner:{
+            type:Schema.Types.ObjectId,
+            ref:"User"
+        }
+    },{timestamps:true})
+
+export const Video = mongoose.model('Video', VideoSchema);
